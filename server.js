bcd8e3509d5d86f78c634843d4827d2e29f2eef5
Abderrazzak <abd1111sakoum@gmail.com>
2021-12-09 15:06:13 +0100

    update

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
-//Install express server
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 4200;
+const server = require('http').Server(app);
+
+
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/client-app-one'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/client-app-one/index.html'));
+});
+ 
+// Start the app by listening on the default Heroku port
+
+
+server.listen(port, function () {
+    console.log("App running on port " + port);
+})
+
+
+
+/*//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -12,4 +36,4 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);*/
