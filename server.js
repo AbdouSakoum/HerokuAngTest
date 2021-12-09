e3d4eb0e1b3c1e2ebdb5597ed9cfc0c3b8a9ee7c
Abderrazzak <abd1111sakoum@gmail.com>
2021-12-09 14:29:18 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// serve only the static files from the dist directory
-app.use(express.static('./dist/client-app-one'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/client-app-one'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/client-app-one/index.html'));
+});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
